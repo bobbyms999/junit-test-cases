620b1ec8cde2d4f7114d38c1636002fba3675270
srivastava <abhishek.srivastava@s-force.org>
2019-07-14 23:43:42 +0530

    junit with intelj

---
+++ src/main/java/com/example/api/junittestcases/JunitTestCasesApplication.java
@@ -0,0 +1,13 @@
+package com.example.api.junittestcases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitTestCasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitTestCasesApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/api/junittestcases/UserRestController.java
@@ -0,0 +1,17 @@
+package com.example.api.junittestcases;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/useraccount")
+public class UserRestController {
+
+    @GetMapping("/get")
+    public String getUserInfo(){
+        return "Hello Hai";
+    }
+
+}
