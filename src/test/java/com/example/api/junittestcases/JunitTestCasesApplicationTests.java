bd16aff82c7a3cc4f71c8b78d139164ed2786def
bobbyms999 <bobbyms999@gmail.com>
2019-07-15 07:38:26 +0530

    modifications added

--- src/test/java/com/example/api/junittestcases/JunitTestCasesApplicationTests.java
+++ src/test/java/com/example/api/junittestcases/JunitTestCasesApplicationTests.java
@@ -1,16 +1,25 @@
 package com.example.api.junittestcases;
 
+import org.junit.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
+
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class JunitTestCasesApplicationTests {
 
+
+
 	@Test
 	public void contextLoads() {
+			Calculator calculator=new Calculator();
+			int output=calculator.add(10,20);
+			Assert.assertEquals(30,output);
 	}
 
 }
